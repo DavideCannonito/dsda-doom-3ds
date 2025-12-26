c643514aca974c9d54d3cb45e725d5e81fd1fd63
Davide Cannonito <52752208+DavideCannonito@users.noreply.github.com>
2025-12-26 08:38:45 +0000

    Fix/segfault (#2)
    * fix: do not stub os level functions
    
    * chore: add vscode tasks and ignore compile_commands.json

--- source/STUBS/i_capture_stubs.c
+++ source/STUBS/i_capture_stubs.c
@@ -3,28 +3,28 @@ int pipe(int* something)
 {
     return -1;
 }
+// these are present in the 3DS libraries. Do not stub them, or else evil segfault appears
+// int fork()
+// {
+//     return -1;
+// }
+// these are present in the 3DS libraries. Do not stub them, or else evil segfault appears
+// void dup2(int fd, int opmode){
+//     return;
+// }
+// these are present in the 3DS libraries. Do not stub them, or else evil segfault appears
+// void close(int fd){
+//     return;
+// }
 
-int fork()
-{
-    return -1;
-}
-
-/*void dup2(int fd, int opmode){
-    return;
-}*/
-
-/*void close(int fd){
-    return;
-}*/
-
-// i have no idea if ... is appropriate
+// // i have no idea if ... is appropriate
 int execl(const char* command,const char* something, ...){
     return -1;
 }
-
-void _exit(int retnum){
-    return;
-}
+// these are present in the 3DS libraries. Do not stub them, or else evil segfault appears
+// void _exit(int retnum){
+//     return;
+// }
 
 void waitpid(int pid, int* something, int returncode){
     return;
